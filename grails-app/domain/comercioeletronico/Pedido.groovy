d8a76b6be80c91482a685ae3b91aae403131d484
Bruno <bruno.eng.comp@gmail.com>
2016-06-16 10:56:08 -0300

    Criar classe Pedido

---
+++ grails-app/domain/comercioeletronico/Pedido.groovy
@@ -0,0 +1,23 @@
+package comercioeletronico
+
+class Pedido {
+    List<ListaProduto> itens = new ArrayList<ListaProduto>()
+    static hasMany = [itens: ListaProduto]
+
+    Date dataPedido
+    static transients = ['total']
+
+    BigDecimal getTotal() {
+        //O inject começa com um valor inicial, no nosso caso 0,
+        //e para cada item da lista (item) vai atualizando o valor
+        //inicial
+        itens.inject(0, { inicial, item ->
+            return inicial + item.precoTotal
+        })
+    }
+
+    static constraints = {
+        //O pedido, quando for criado, deve ter ao menos 1 item
+        itens minSize: 1
+    }
+}
