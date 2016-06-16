c1f1e9dad5a2b1b786c08c14306245ae2a14b7c2
Bruno <bruno.eng.comp@gmail.com>
2016-06-16 10:49:37 -0300

    Adicionar método no controller CarrinhoComprasController para inserir produtos no carrinho

--- grails-app/controllers/comercioeletronico/CarrinhoComprasController.groovy
+++ grails-app/controllers/comercioeletronico/CarrinhoComprasController.groovy
@@ -1,5 +1,7 @@
 package comercioeletronico
 
+import grails.converters.JSON
+
 class CarrinhoComprasController {
 
     //Podemos alterar o escopo do controller para session (por padrão é singleton)
@@ -15,13 +17,33 @@ class CarrinhoComprasController {
     //http://localhost:8080/carrinhoCompras.json
     static responseFormats = ['html', 'json', 'xml']
 
+    //O allowedMethods define que tipo de método HTTP cada uma das nossas
+    //actions consegue responder. Se o usuário tentar mandar um outro tipo
+    //de método HTTP, receberá uma resposta 405 (Method Not Allowed)
+    static allowedMethods = [index: 'GET', adicionarProduto: 'POST']
+
     //Variável do controller
     CarrinhoCompras carrinhoCompras = new CarrinhoCompras()
+
+    //Bean utilizado para mostrar a mensagem adequada para cada tipo de erro
+    def messageSource
 
     def index() {
         //Por convenção, ao utilizar o respond, o Grails procura uma view index,
         //como não existe uma view gsp, a view JSON que definimos é encontrada
         //O carrinhoCompras é utilizado como model da view
         respond carrinhoCompras
+    }
+
+    def adicionarProduto(ListaProduto listaProduto) {
+        if (!listaProduto.validate()) {
+            def erros = listaProduto.errors.allErrors.inject([], { listaErros, erro ->
+                (listaErros << messageSource.getMessage(erro, null))
+            })
+            render erros as JSON
+            return
+        }
+        carrinhoCompras.adicionarProduto(listaProduto)
+        render([mensagem: "Produto adicionado com sucesso"] as JSON)
     }
 }
