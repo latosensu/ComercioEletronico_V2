cffa102f0ed7de6d0eccc53c685da99b7adf84ea
Bruno <bruno.eng.comp@gmail.com>
2016-06-16 10:37:45 -0300

    Alterar escopo do controller CarrinhoComprasController para session

--- grails-app/controllers/comercioeletronico/CarrinhoComprasController.groovy
+++ grails-app/controllers/comercioeletronico/CarrinhoComprasController.groovy
@@ -2,6 +2,13 @@ package comercioeletronico
 
 class CarrinhoComprasController {
 
+
+    //Podemos alterar o escopo do controller para session (por padrão é singleton)
+    //O escopo session mantém as variáveis do controller na sessão do usuário
+    //O singleton apresenta as mesmas variáveis para todos os usuários
+    //O outro escopo é o prototype e a variável só vale para uma determinada request
+    static scope = "session"
+
     //Variável do controller
     CarrinhoCompras carrinhoCompras = new CarrinhoCompras()
 
