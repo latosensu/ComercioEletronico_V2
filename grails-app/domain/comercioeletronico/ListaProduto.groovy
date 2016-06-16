ff94d7241a70f3fe51122a663cc25b0aea6b0765
Bruno <bruno.eng.comp@gmail.com>
2016-06-16 00:01:30 -0300

    Adicionar propriedade transiente e seu get

--- grails-app/domain/comercioeletronico/ListaProduto.groovy
+++ grails-app/domain/comercioeletronico/ListaProduto.groovy
@@ -5,6 +5,15 @@ class ListaProduto {
     Produto produto
     Integer quantidade
 
+    //Propriedade transiente, ou seja, não é persistida no banco de dados
+    static transients = ['precoTotal']
+
+    //Get para a propriedade transiente, para o Grails saber como calcular
+    //o valor da propriedade
+    BigDecimal getPrecoTotal() {
+        return quantidade * produto.preco
+    }
+
     static constraints = {
     }
 }
